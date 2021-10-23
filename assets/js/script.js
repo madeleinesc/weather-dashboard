72d71eef150cd23319124b52ed582c05859cb999
Madeleinesc <madeleine.s.cosway@gmail.com>
2021-10-23 11:49:31 +1030

    moment.js for current day and date in jumbotron

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 var button = document.querySelector('button')
 
 var apiKey = "78196b2ec1caf4656d01a3423931079a"; // key to access weather api's
+
+// TODAY'S DAY AND DATE FOR JUMBOTRON 
+// set current date and time 
+var currentDay = moment().format("dddd, MMMM Do");
+
+// display current date/time 
+$("#currentDay").text(currentDay);
